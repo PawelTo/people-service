fe2e04d79c4a6c3ffff8f72e85d42c9e5b0ae246
PawelTo <pawel.toporowicz@gmail.com>
2020-12-01 23:24:55 +0100

    remove cqrs configuration

--- src/main/java/pl/pawel/cqrs/domain/UserCommandService.java
+++
@@ -1,8 +0,0 @@
-package pl.pawel.cqrs.domain;
-
-import java.util.concurrent.CompletableFuture;
-
-public interface UserCommandService {
-
-    public CompletableFuture<Object> create(User user);
-}
