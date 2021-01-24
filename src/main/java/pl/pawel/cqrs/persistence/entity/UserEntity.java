ec97d810f3c846cfd2bb0e91786ab4be726e237f
PawelTo <pawel.toporowicz@gmail.com>
2021-01-24 21:51:16 +0100

    optimize imports

--- src/main/java/pl/pawel/cqrs/persistence/entity/UserEntity.java
+++ src/main/java/pl/pawel/cqrs/persistence/entity/UserEntity.java
@@ -5,10 +5,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-
-import static javax.persistence.GenerationType.IDENTITY;
 
 @AllArgsConstructor
 @Data
