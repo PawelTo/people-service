3cd119caba85511043f55f926d177a3ee8cbb50f
PawelTo <pawel.toporowicz@gmail.com>
2021-01-26 02:11:33 +0100

    provide custom personAggregateStatistics for JPQL query

--- src/main/java/pl/pawel/cqrs/persistence/repository/PersonEntityRepository.java
+++ src/main/java/pl/pawel/cqrs/persistence/repository/PersonEntityRepository.java
@@ -1,7 +1,19 @@
 package pl.pawel.cqrs.persistence.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import pl.pawel.cqrs.persistence.entity.PersonEntity;
+import pl.pawel.cqrs.persistence.aggregatestatistic.PersonAggregateStatistics;
+
+import java.util.Optional;
 
 public interface PersonEntityRepository extends JpaRepository<PersonEntity, PersonEntity.PersonKey> {
+
+    @Query(value ="SELECT new pl.pawel.cqrs.persistence.aggregatestatistic.PersonAggregateStatistics(" +
+                 "p.name, "+
+                 "p.salary as avgSalary"+
+                 ")"+
+                 " FROM person p"
+    )
+    Optional<PersonAggregateStatistics> countAverageSalary();
 }
