4cf07fb3b6ece2caa0276cddfc9b0f8ed9bedd84
PawelTo <pawel.toporowicz@gmail.com>
2021-02-23 02:58:31 +0100

    add custom bean validation for date of birth

--- src/main/java/pl/pawel/cqrs/controllers/V1PeopleController.java
+++ src/main/java/pl/pawel/cqrs/controllers/V1PeopleController.java
@@ -1,28 +1,37 @@
 package pl.pawel.cqrs.controllers;
 
+import static org.springframework.http.MediaType.APPLICATION_JSON_VALUE;
+
 import io.swagger.v3.oas.annotations.tags.Tag;
+import java.util.List;
+import javax.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.*;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 import pl.pawel.cqrs.controllers.form.PersonForm;
 import pl.pawel.cqrs.controllers.view.PersonView;
 import pl.pawel.cqrs.persistence.aggregatestatistic.PersonAggregateStatistics;
 import pl.pawel.cqrs.service.PersonService;
 
-import java.util.List;
-
 @RequestMapping(V1PeopleController.API_PATH)
 @RequiredArgsConstructor
 @RestController
 @Tag(name = V1PeopleController.API_PATH)
+@Validated
 public class V1PeopleController {
 
     public static final String API_PATH = "/api/v1/people";
 
     private final PersonService personService;
 
-    @PostMapping
-    public PersonView add(@RequestBody PersonForm personForm) {
+    @PostMapping(produces = APPLICATION_JSON_VALUE)
+    public PersonView add(@RequestBody @Valid PersonForm personForm) {
         return personService.createPerson(personForm);
     }
 
